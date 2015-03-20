f1003ab6ace5901232009923bfbc8b7122f08b71
Michael Fairhurst <michaelrfairhurst@gmail.com>
2015-03-20 14:56:43 -0700

    fix missing aliases

--- common_rc.sh
+++ common_rc.sh
@@ -6,3 +6,6 @@ alias g=git
 alias sc=systemctl
 alias s="sudo " # allow moar aliases latar
 alias sudo="sudo " # allow moar aliases latar # allow moar aiases latar
+
+alias ssh='~/.mikeprompt/promptexp.sh ssh'
+alias su='~/.mikeprompt/promptexp.sh su'
